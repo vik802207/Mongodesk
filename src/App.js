c07d2c4156bab577886c3256727b41e6c789cdfb
Vikash Gupta <137061206+vik802207@users.noreply.github.com>
2025-08-17 07:50:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useRef } from 'react';
 
-const API_BASE = 'http://localhost:8080';
+const API_BASE = 'https://mongodesk.onrender.com/';
 
 export default function App() {
   const [transcript, setTranscript] = useState('');
